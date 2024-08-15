4df67d83ec58028dba6fd2120dac25df9836b7af
unknown <muhammadnuryusufjonov31@gmail.com>
2024-08-15 17:57:26 +0500

    models created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    narxi = models.FloatField()
+    discount = models.PositiveIntegerField()
+    rang = models.CharField(max_length=50, null=True, blank=True)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='product_images')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
